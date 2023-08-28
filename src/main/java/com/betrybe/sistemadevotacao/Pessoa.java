e14e3c7835529992a50f383ed9b1faff87b93b09
Natalia de Oliveira <109634929+natooliv@users.noreply.github.com>
2023-08-28 10:07:51 -0300

    Requisito 01 e 02

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -3,11 +3,21 @@ package com.betrybe.sistemadevotacao;
 public abstract class Pessoa {
   protected String nome;
 
+  // Construtor padrão
+  public Pessoa() {
+    this.nome = "";
+  }
+
+  // Construtor com nomeCompleto
+  public Pessoa(String nomeCompleto) {
+    this.nome = nomeCompleto;
+  }
+
   public String getNome() {
     return nome;
   }
 
-  public void setNome(String nome) {
-    this.nome = nome;
+  public void setNome(String nomeCompleto) {
+    this.nome = nomeCompleto;
   }
 }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,40 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
 
+public class PessoaCandidata extends Pessoa {
+
+  private int identificadorVoto;     // identificador para votação
+  private int quantidadeVotos;       // quantidade de votos recebidos pela pessoa candidata
+
+  /**
+   * Construtor para a classe PessoaCandidata.
+   *
+   * @param nomeCompleto Nome completo da pessoa candidata.
+   * @param identificadorVoto Identificador para votação da pessoa candidata.
+   */
+  public PessoaCandidata(String nomeCompleto, int identificadorVoto) {
+    super(nomeCompleto);           // chama o construtor da classe base
+    this.identificadorVoto = identificadorVoto;
+    this.quantidadeVotos = 0;      // inicializa quantidadeVotos com zero
+  }
+
+
+  public int getIdentificadorVoto() {
+    return identificadorVoto;
+  }
+
+
+  public void setIdentificadorVoto(int identificadorVoto) {
+    this.identificadorVoto = identificadorVoto;
+  }
+
+
+  public int getQuantidadeVotos() {
+    return quantidadeVotos;
+  }
+
+  public void receberVoto() {
+    this.quantidadeVotos++;
+  }
 }
+
